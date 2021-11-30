02027d03bfd1f9e0129998f9a6653401f5e45a5f
Taciano Morais Silva <tacianosilva@gmail.com>
2021-11-30 17:46:47 -0300

    Add a lib morgan para logging

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
+import express, { Application } from 'express';
+import morgan from 'morgan';
 import { useRoutes } from './routes';
 
 const PORT = process.env.PORT || 8091;
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
+
 useRoutes(app);
 
-app.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}.`));
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}.`)
+});
